6faff44b55b43a1f3942ccea536a1366732ec166
saadabban76 <115649011+saadabban76@users.noreply.github.com>
2023-03-28 23:57:18 +0530

    test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className="text-gray-700 text-3xl font-semibold">
+        <h1 className="text-gray-700 border-b border-gray-900 text-3xl font-semibold">
           Decognito FrontEnd
         </h1>
       </main>
